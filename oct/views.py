b54affc6c82fcdaff0a36cd6e72929aaeb33f31a
Atik <atikrobian@gmail.com>
2019-11-08 20:02:21 +0600

    Edited few files

--- oct/views.py
+++ oct/views.py
@@ -26,7 +26,7 @@ def udpatematch(request,id):
     session.trust_env = False
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36', 'content-type': 'application/xhtml+xml'}
     endpoint = f'https://xml.sportspunter.com/xml?username=bmalpass&password=malpass82&since={ timestamp }&cid={ id }'
-    res = session.get(endpoint, headers=headers, timeout=5)
+    res = session.get(endpoint, headers=headers)
     soup = BeautifulSoup(res.content, 'html.parser')
     matches = {}
     outrights = []
@@ -63,7 +63,34 @@ def udpatematch(request,id):
                                 odd_val = ''
                             odds_data.append([mid, opt_name, odd_val])
         odds_data.sort()
-        matches[match_name]=[ht,at,tp,odds_data]
+        BL = [item[0] for item in odds_data]
+        BL = list(set(BL))
+        OL = [item[1] for item in odds_data]
+        OL = list(set(OL))
+        master_list = []
+        for o in OL:
+            x = [o]
+            for b in BL:
+                total = 0
+                for l in odds_data:
+                    if l[0] == b and l[1] == o and l[2] != '':
+                        total += float(l[2].strip())
+                x.append(format(total, '.2f'))
+            master_list.append(x)
+        lr = []
+        i = 0
+        for col in BL:
+            i += 1
+            lrtotal = 0
+            for item in master_list:
+                if item[i] != '':
+                    lrtotal += (float(item[i])/100)
+            lr.append(format(lrtotal, '.2f'))
+        BL.insert(0,"")
+        master_list.insert(0,BL)
+        lr.insert(0,'Market %')
+        master_list.append(lr)
+        matches[match_name]=[ht,at,tp,master_list]
     for outright in soup.select('outrights market'):
         market_name = outright.get('name')
         for opt in outright.select('sel'):
